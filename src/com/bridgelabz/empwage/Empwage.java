6336016f045a6f8e3cd6e66602d0c899bda15543
shubhamagrawal902785 <shubhamag937@gmail.com>
2022-10-22 17:34:08 +0530

    Added: Empwage part time

--- src/com/bridgelabz/empwage/Empwage.java
+++ src/com/bridgelabz/empwage/Empwage.java
@@ -2,29 +2,32 @@ package com.bridgelabz.empwage;
 
 public class Empwage {
 	
-	public static void main(String[] args) {
-        //Use case 1
 
-        System.out.println("Welcome to Employee wage");
+	    public static void main(String[] args) {
+	        //Use case 3 added the option of part time
 
-        int empCheck = (int)(Math.random() * 2);
-        int wage = 0;
-        int isFulltime = 1;
-        int empHrs = 0;
-        int empWageperHrs = 20;
+	        System.out.println("Welcome to Employee wage");
 
-        if(empCheck==isFulltime){
-            System.out.println("The employee is present");
-            empHrs = 8;
+	        int empCheck = (int)(Math.random() * 3); //Generating Random Number
+	        int wage = 0;
+	        int isFulltime = 1;
+	        int empHrs = 0;
+	        int empWageperHrs = 20;
+	        int isPartTime = 2;
 
+	        if(empCheck==isFulltime){
+	            System.out.println("The employee is doing full time");
+	            empHrs = 8;
 
-        }else{
-            System.out.println("Employee is Absent");
-            empHrs = 0;
-        }
+	        }else if(empCheck==isPartTime){
+	            System.out.println("Employee is doing part time");
+	            empHrs = 4;
+	        }else{
+	            System.out.println("Employee is Absent");
+	            empHrs = 0;
+	        }
 
-        wage = empHrs * empWageperHrs;
-        System.out.println("The employee wage is  "+ wage);
-    }
-}
-
+	        wage = empHrs * empWageperHrs;
+	        System.out.println("The employee wage is  "+ wage);
+	    }
+	}

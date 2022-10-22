d71d8a5271754db3ffac7cbe8d4cef410e193013
shubhamagrawal902785 <shubhamag937@gmail.com>
2022-10-22 17:15:44 +0530

    Welcome to employee present or absent

---
+++ src/com/bridgelabz/empwage/Empwage.java
@@ -0,0 +1,20 @@
+package com.bridgelabz.empwage;
+
+public class Empwage {
+	
+	    public static void main(String[] args) {
+	        //Use case 1
+	        
+	        System.out.println("Welcome to Employee wage");
+
+	        int a = (int)(Math.random() * 2);
+
+	        if(a==1){
+	            System.out.println("The employee is present");
+	        }else{
+	            System.out.println("Employee is Absent");
+	        }
+	    }
+	}
+
+
